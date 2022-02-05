4003938a33a729b3fab3567ec7cfd676923ea2c0
Harshks2406 <harshsingh.hks@gmail.com>
2022-02-05 21:43:28 +0530

    backend added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var Userdb = require('./src/models/user')
 const axios = require('axios')
 
 app.use(express.json());
-app.use(bodyparser.urlencoded({extended: true}))
+// app.use(bodyparser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}))
 
 const static_Path = path.join(__dirname,"./public")
@@ -25,8 +25,17 @@ app.use(express.static(static_Path))
 
 connectDB();
 
-app.get('/',(req,res)=>{
-    axios.get('http://localhost:3535/find')
+//hbs helper
+hbs.registerHelper('if_eq', function(a, b, opts) {
+    if (a == b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
+app.get('/',(req,res)=>{    
+    axios.get(`http://localhost:${port}/find`)
      .then(function(response){
         res.render('index.hbs',{users:response.data})
      })
@@ -40,46 +49,105 @@ app.get('/newUser',(req,res)=>{
 })
 
 app.get('/updateUser',(req,res)=>{
-    res.render('updateUser.hbs')
+    axios.get(`http://localhost:${port}/find`,{params:{id:req.query.id}})
+     .then(function(userData){
+        res.render('updateUser.hbs',{user:userData.data})
+     })
+     .catch(err=>{
+         res.send(err)
+     })
 })
 
-app.post('/newUser',(req,res)=>{
-    console.log(req.body)
-    if(!req.body){
-        res.status(400).send({message: "Content cannot be empty!"})
-        return
-    }
-    const user = new Userdb({
-        name: req.body.name,
-        email: req.body.email,
-        gender: req.body.gender,
-        status: req.body.status
-    })
-    user
-        .save(user)
-        .then(data=>{
-            res.status(200).redirect('/')
-        })
-        .catch((err)=>{
-            res.status(500).send({message: err.message || "Some error occured while creating a create operation"})
-        })
-})
+// app.post('/newUser',(req,res)=>{
+//     console.log(req.body)
+//     if(!req.body){
+//         res.status(400).send({message: "Content cannot be empty!"})
+//         return
+//     }
+//     const user = new Userdb({
+//         name: req.body.name,
+//         email: req.body.email,
+//         gender: req.body.gender,
+//         status: req.body.status
+//     })
+//     user
+//         .save(user)
+//         .then(data=>{
+//             res.status(200).redirect('/')
+//         })
+//         .catch((err)=>{
+//             res.status(500).send({message: err.message || "Some error occured while creating a create operation"})
+//         })
+// })
 
 //Both of them are correct
 
-// app.post('/newUser',async(req,res)=>{
-//     try {
-//         const user = new Userdb(req.body)
-//         console.log(user)
-//         result = await user.save()
-//         console.log(result)
-//         res.status(201).render('index.hbs')
-//     } catch (error) {
-//         res.status(500).send(error)
-//     }
-// })
+app.post('/newUser',async(req,res)=>{
+    try {
+        const user = new Userdb(req.body)
+        console.log(user)
+        result = await user.save()
+        console.log(result)
+        res.status(201).redirect('/')
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.post('/updateUser/:id',(req,res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message:'Data to update cannot be empty'})
+    }
+
+    const id = req.params.id
+    console.log(req.body)
+    Userdb.findByIdAndUpdate(id,req.body,{usefindAndModify:false, new:true})
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message:`Cannot Update user with ${id}.`})
+            }else{
+                res.status(201).redirect('/')
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({err, message: "Erro Update user Information"})
+        })
+
+})
+
+app.get('/delete/:id',(req,res)=>{
+    const id = req.params.id
+    Userdb.findByIdAndDelete(id)
+     .then(data=>{
+         if(!data){
+             res.status(404).send({message:`Cannot delete with id ${id}. Maybe id is wrong`}).redirect('/')
+         }else{
+             res.status(201).redirect('/')
+         }
+     })
+     .catch(err=>{
+         res.status(500).send({message: err || `Could not delete user with id ${id}`})
+     })
+})
 
 app.get('/find',(req,res)=>{
+
+    if(req.query.id){
+    const id=req.query.id
+    Userdb.findById(id)
+     .then(data=>{
+         if(!data){
+             res.status(404).send({message: "Not found user"})
+         }else{
+             res.send(data)
+         }
+     })
+     .catch(err=>{
+         res.status(500).send({message:"Error occured"})
+     })
+}else{
     Userdb.find()
      .then(user=>{
         res.send(user)
@@ -87,21 +155,7 @@ app.get('/find',(req,res)=>{
      .catch(err=>{
          res.status(500).send({message: err.message || "Error occured"})
      })
-})
-
-app.get('/delete',(req,res)=>{
-    const id = req.params.id
-    Userdb.findByIdAndDelete(id)
-     .theh(data=>{
-         if(!data){
-             res.status(404).send({message:`Cannot delete with id ${id}. Maybe id is wrong`})
-         }else{
-             res.send({message: "User was deleted successfully"})
-         }
-     })
-     .catch(err=>{
-         res.status(500).send({message: `Could not delete user with id ${id}`})
-     })
+    }
 })
 
 app.listen(port,()=>{
